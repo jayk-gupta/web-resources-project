2e26bede99b41a357f66fcd5629f38630834a8e9
Jay kumar gupta <jayesh.gupta3224@gmail.com>
2023-05-14 20:38:10 +0530

    added div for space in video files

--- src/components/videos/HtmlV.jsx
+++ src/components/videos/HtmlV.jsx
@@ -18,7 +18,7 @@ export const HtmlV = () => {
       <div className="grid grid-cols-3 gap-x-12 gap-y-16  px-36 my-12">
         {cardComponents}
       </div>
-      <div className=" w-full h-[20rem]">
+      <div className="p-4 w-full h-[20rem]">
        
       </div>
     </>
--- src/components/videos/JavascriptV.jsx
+++ src/components/videos/JavascriptV.jsx
@@ -14,8 +14,11 @@ export const JavascriptV = () => {
     />
   ));
   return (
-    <div className="grid grid-cols-3 gap-x-12 gap-y-16  px-36 my-12">
-      {cardComponents}
-    </div>
+    <>
+      <div className="grid grid-cols-3 gap-x-12 gap-y-16  px-36 my-12">
+        {cardComponents}
+      </div>
+      <div className="p-4 w-full h-[20rem]"></div>
+    </>
   );
 };

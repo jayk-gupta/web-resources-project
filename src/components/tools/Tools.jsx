eb7b4f37ac55e460d301f616922d11e488840b30
Jay kumar gupta <jayesh.gupta3224@gmail.com>
2023-05-15 14:01:56 +0530

    added tools of js ans react

--- src/components/tools/Tools.jsx
+++ src/components/tools/Tools.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-// import TagsNav from "../nav/TagsNav";
+import TagsNav from "../nav/TagsNav";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
@@ -12,26 +12,7 @@ const Tools = () => {
     >
       {/* <TagsNav /> */}
 
-      <nav className="categories text-lg py-2 bg-[#f3d9fa] flex  gap-12 text-[#562c35] px-28 sticky top-0">
-        {/* <Link to="all-categories" className="hover:text-orange-500 font-roboto">
-        <p className="tracking-wider hover:cursor-pointer hover">
-          All Categories{" "}
-        </p>
-      </Link> */}
-        <h2 className="text-xl font-merriweather tracking-wider mr-12">
-          Web Dev Resources
-        </h2>
-        <Link to="html" className="hover:text-orange-500 font-roboto">
-          <p className="tracking-wider hover:cursor-pointer hover:text-[#d63afd]">
-            HTML
-          </p>
-        </Link>
-        <Link to="css" className="hover:text-orange-500 font-roboto">
-          <p className="tracking-wider hover:cursor-pointer hover:text-[#d63afd]">
-            CSS
-          </p>
-        </Link>
-      </nav>
+      <TagsNav />
 
       <div className="m-auto text-center pt-6">
         <h2 className="text-4xl font-merriweather tracking-wider mb-2">

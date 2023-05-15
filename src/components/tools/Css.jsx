0f0322a4bb927233e4a2a9398a3bf13770ac164a
Jay kumar gupta <jayesh.gupta3224@gmail.com>
2023-05-15 10:51:39 +0530

    added tools images

--- src/components/tools/Css.jsx
+++ src/components/tools/Css.jsx
@@ -11,11 +11,15 @@ export const Css = () => {
       link={item.link}
       description={item.description}
       i={i}
+            img={item.img}
     />
   ));
   return (
-    <div className="grid grid-cols-3 gap-x-12 gap-y-16  px-36 my-12">
-      {cardComponents}
-    </div>
+    <>
+      <div className="grid grid-cols-3 gap-x-12 gap-y-16  px-36 my-12">
+        {cardComponents}
+      </div>
+      <div className="p-4 w-full h-[20rem]"></div>
+    </>
   );
 };

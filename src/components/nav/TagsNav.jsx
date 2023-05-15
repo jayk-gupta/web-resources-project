fadd666d6fada99a1f473beacb9ad07bb2e0b90d
Jay kumar gupta <jayesh.gupta3224@gmail.com>
2023-05-15 20:18:26 +0530

    added tags hover effect

--- src/components/nav/TagsNav.jsx
+++ src/components/nav/TagsNav.jsx
@@ -1,37 +1,36 @@
 import React from 'react'
-import { Link} from "react-router-dom";
-
+import { NavLink} from "react-router-dom";
+import "./TagsNav.css";
 const TagsNav = () => {
   return (
     <nav className="categories text-lg py-2 bg-[#f3d9fa] flex  gap-12 text-[#562c35] px-28 sticky top-0">
-      {/* <Link to="all-categories" className="hover:text-orange-500 font-roboto">
-        <p className="tracking-wider hover:cursor-pointer hover">
-          All Categories{" "}
-        </p>
-      </Link> */}
       <h2 className="text-xl font-merriweather tracking-wider mr-12">
         Web Dev Resources
       </h2>
-      <Link to="html" className="hover:text-orange-500 font-roboto">
-        <p className="tracking-wider hover:cursor-pointer hover:text-[#d63afd]">
-          HTML
-        </p>
-      </Link>
-      <Link to="css" className="hover:text-orange-500 font-roboto">
-        <p className="tracking-wider hover:cursor-pointer hover:text-[#d63afd]">
-          CSS
-        </p>
-      </Link>
-      <Link to="javascript" className="hover:text-orange-500 font-roboto">
-        <p className="tracking-wider hover:cursor-pointer hover:text-[#d63afd]">
-          Javascript
-        </p>
-      </Link>
-      <Link to="React" className="hover:text-orange-500 font-roboto">
-        <p className="tracking-wider hover:cursor-pointer hover:text-[#d63afd]">
-          React
-        </p>
-      </Link>
+      <NavLink
+        to="html"
+        className="tracking-wider hover:cursor-pointer hover:text-[#d63afd] "
+      >
+        HTML
+      </NavLink>
+      <NavLink
+        to="css"
+        className="tracking-wider hover:cursor-pointer hover:text-[#d63afd] "
+      >
+        CSS
+      </NavLink>
+      <NavLink
+        to="javascript"
+        className="tracking-wider hover:cursor-pointer hover:text-[#d63afd] "
+      >
+        Javascript
+      </NavLink>
+      <NavLink
+        to="React"
+        className="tracking-wider hover:cursor-pointer hover:text-[#d63afd] "
+      >
+        React
+      </NavLink>
     </nav>
   );
 }

19f97c6023dad29cb52197967de3ccf15aa0395e
Vilas Chavhan <50195101+villAsh@users.noreply.github.com>
2023-05-22 12:23:09 +0530

    #115 : made homepage responsive on smaller screens
    Home page was not responsive on smaller screens, I have done some changes now it looks good on smaller screens.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,34 @@
-import { motion } from "framer-motion";
-
+import { motion } from "framer-motion"
 
 export default function Home() {
   return (
-    <div className=" w-full  flex mx-auto ml-16 mt-6">
+    <div className="h-screen w-full flex flex-row items-center justify-center md:ml-16 md:mt-6">
       <motion.div
-        className=" w-3/5 p-12 flex flex-col gap-8"
+        className=" p-12 flex flex-col space-y-0"
         initial={{ opacity: 0, translateX: -100 }}
         animate={{ opacity: 1, translateX: 0 }}
         transition={{ duration: 0.5 }}
       >
-        <h1 className="text-7xl text-[#4b005e] leading-[5rem] tracking-wider font-lobster drop-shadow-xl ">
+        <h1 className="text-4xl md:text-7xl text-[#4b005e] leading-[2.5rem] md:leading-[5rem] tracking-wider font-lobster drop-shadow-xl ">
           Web Resources Hub
         </h1>
-        <h2 className="text-4xl text-[#4b005e] leading-[4rem] tracking-wider font-merriweather  drop-shadow-xl">
+        <h2 className="text-xl md:text-4xl text-[#4b005e] leading-[1.8rem] md:leading-[4rem] tracking-wider font-merriweather  drop-shadow-xl">
           Get Empowered <span className=" text-[]"> with </span> <br />
           Web Development
           <br />
           <span className=" text-[]"> Resources </span>
         </h2>
-        <p className="text-lg w-4/5 text-[#862e9c]">
+        <p className="text-base md:text-lg w-4/5 text-[#862e9c] text-justify">
           Explore our vast collection of web development resources,
           <br></br>
-          designed to
-          give you the knowledge and tools you need to
-          <br/>
-          succeed in the world of
-          web development!
+          designed to give you the knowledge and tools you need to
+          <br />
+          succeed in the world of web development!
         </p>
       </motion.div>
 
-      <div className="flex items-center w-1/2">
+      <div className="hidden sm:flex items-center w-1/2">
         <img
-          className="w-full"
           src="https://user-images.githubusercontent.com/100681165/238192164-570dad2f-ba6d-4a35-8ae0-7fb364560653.svg"
           alt="logo"
         />
@@ -44,5 +40,5 @@ export default function Home() {
         ></img>
       </div>
     </div>
-  );
+  )
 }

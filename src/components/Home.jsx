41a238bd60c61b5412b8401b92f784e41938ce76
monishka8303 <monishka8303@gmail.com>
2023-05-30 12:22:11 +0530

    Responsiveness Issue Fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,31 +2,31 @@ import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
-    <div className=" w-full  flex mx-auto ml-12 mt-6">
+    <div className=" w-full flex mx-auto md:ml-8 lg:ml-12 mt-6">
       <motion.div
-        className=" w-3/5 p-6 flex flex-col gap-8"
+        className="sm:w-3/5 sm:p-4 md:p-6 flex flex-col gap-8 md:gap-6"
         initial={{ opacity: 0, translateX: -100 }}
         animate={{ opacity: 1, translateX: 0 }}
         transition={{ duration: 0.5 }}
       >
-        <h1 className="text-7xl text-[#4b005e] leading-[5rem] tracking-wider font-lobster drop-shadow-xl ">
+        <h1 className="text-5xl md:text-6xl xl:text-7xl text-[#4b005e] leading-[3rem] md:leading-[4rem] xl:leading-[5rem] md:tracking-normal lg:tracking-wider font-lobster drop-shadow-xl ">
           Web Resources Hub
         </h1>
-        <h2 className="text-4xl text-[#4b005e] leading-[4rem] tracking-wider font-merriweather  drop-shadow-xl">
+        <h2 className="text-xl md:text-2xl xl:text-4xl text-[#4b005e] leading-[2rem] md:leading-[3rem] xl:leading-[4rem] md:tracking-wide lg:tracking-wider font-merriweather  drop-shadow-xl">
           Get Empowered <span className=" text-[]"> with </span> <br />
           Web Development
           <br />
           <span className=" text-[]"> Resources </span>
         </h2>
-        <p className="text-lg w-4/5 text-[#862e9c]">
+        <p className="text-sm md:text-md lg:text-lg xl:text-xl w-4/5 text-[#862e9c]">
           Explore our vast collection of web development resources,
           designed to give you the knowledge and tools you need to
-          <br />
+          
           succeed in the world of web development!
         </p>
       </motion.div>
 
-      <div className="flex items-center w-1/2">
+      <div className="hidden sm:flex bottom-0 items-center w-1/2">
         <img
           className="w-full"
           src="https://user-images.githubusercontent.com/100681165/238192164-570dad2f-ba6d-4a35-8ae0-7fb364560653.svg"

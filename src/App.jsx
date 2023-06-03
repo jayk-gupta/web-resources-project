103e1accdc80b977caaeb67b2dc469a8a7cc1bf9
kadambari68 <kadambari.21jics197@jietjodhpur.ac.in>
2023-06-04 00:25:58 +0530

    highlight the selected icon when its respective page is opened

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes,NavLink } from 'react-router-dom';
+import { Route, Routes, NavLink, useLocation } from 'react-router-dom';
 import Nav from './components/nav/Nav.jsx';
 import Home from './components/Home.jsx';
 import Book from './components/books/Index.jsx'
@@ -9,24 +9,63 @@ import Websites from './components/websites/Index.jsx';
 import Challenges from './components/challenges/Index.jsx';
 import { BsGithub } from "react-icons/bs";
 
-
 const App = () => {
+  const location = useLocation();
+
   return (
     <div className="flex">
       <Nav />
       <div className="mx-20">
         <Routes location={location} key={location.key}>
           {/* Home */}
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
           <Route path="/Videos" element={<Videos />} />
           <Route path="/Websites" element={<Websites />} />
           <Route path="/Challenges" element={<Challenges />} />
           <Route path="/Books" element={<Book />} />
           <Route path="/Tools" element={<Tools />} />
         </Routes>
-
       </div>
     </div>
+  );
+};
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact to="/" activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Videos" activeClassName="active">
+            Videos
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Websites" activeClassName="active">
+            Websites
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Challenges" activeClassName="active">
+            Challenges
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Books" activeClassName="active">
+            Books
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Tools" activeClassName="active">
+            Tools
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 

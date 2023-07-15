a51c5b24ef3b4692eae3e4a62e660aa2610d30e1
adarshgupta404 <gadarsh747@gmail.com>
2023-07-15 07:41:43 +0530

    added animated nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React from "react";
-import { Route, Routes, NavLink } from "react-router-dom";
-// import Nav from './components/nav/Nav.jsx';
-import SideNav from "./components/nav/SideNav.jsx";
-import Home from "./components/Home.jsx";
-import Book from "./components/books/Index.jsx";
-import Tools from "./components/tools/Index.jsx";
-import Videos from "./components/videos/Index.jsx";
-import Websites from "./components/websites/Index.jsx";
-import Challenges from "./components/challenges/Index.jsx";
+import React from 'react';
+import { Route, Routes,NavLink } from 'react-router-dom';
+import Nav from './components/nav/Nav.jsx';
+import Home from './components/Home.jsx';
+import Book from './components/books/Index.jsx'
+import Tools from './components/tools/Index.jsx';
+import Videos from './components/videos/Index.jsx';
+import Websites from './components/websites/Index.jsx';
+import Challenges from './components/challenges/Index.jsx';
+import { BsGithub } from "react-icons/bs";
+
 
 const App = () => {
   return (
     <div className="flex">
-      <SideNav className="" />
-      <div className="pl-60">
+      <Nav />
+      <div className="mx-20">
         <Routes location={location} key={location.key}>
           {/* Home */}
           <Route path="/" element={<Home />}></Route>
@@ -23,6 +24,7 @@ const App = () => {
           <Route path="/Books" element={<Book />} />
           <Route path="/Tools" element={<Tools />} />
         </Routes>
+
       </div>
     </div>
   );

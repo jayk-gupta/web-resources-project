6f97c3bdd01de853ca67f7908fd531a73ab3706e
adarshgupta404 <gadarsh747@gmail.com>
2023-07-24 21:42:39 +0530

    minor changes

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,76 +1,169 @@
 import React from "react";
-import { FaHome } from "react-icons/fa";
-import { BsFillCameraVideoFill } from "react-icons/bs";
-import { FaLaptopCode } from "react-icons/fa";
-import { CgWebsite } from "react-icons/cg";
 import { NavLink } from "react-router-dom";
-import { BsTools } from "react-icons/bs";
-import { BsBookHalf } from "react-icons/bs";
+// import { GiSwordsEmblem } from "react-icons/gi";
+import {LuSwords} from 'react-icons/lu'
+import { FaBars, FaHome,FaTools } from "react-icons/fa";
+import { FaBook } from 'react-icons/fa';
+import { BsCameraVideoFill} from "react-icons/bs"
+import { useState } from "react";
+import {CgWebsite} from 'react-icons/cg';
+import './Nav.css';
+import { AnimatePresence, motion } from "framer-motion";
 
-const Nav = () => {
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    icon: <FaHome size={30} />,
+  },
+  {
+    path: "/Videos",
+    name: "Videos",
+    icon: <BsCameraVideoFill size={30} />,
+  },
+  {
+    path: "/Websites",
+    name: "Websites",
+    icon: <CgWebsite size={30} />,
+  },
+  {
+    path: "/Challenges",
+    name: "Challenges",
+    icon: <LuSwords size={30} />,
+  },
+  {
+    path: "/Books",
+    name: "Books",
+    icon: <FaBook size={30} />,
+  },
+  {
+    path: "/Tools",
+    name: "Tools",
+    icon: <FaTools size={30} />,
+  },
+];
+
+const Nav = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const inputAnimation = {
+    hidden: {
+      width: 0,
+      padding: 0,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    show: {
+      width: "140px",
+      padding: "5px 15px",
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
+  const showAnimation = {
+    hidden: {
+      width: 0,
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    show: {
+      opacity: 1,
+      width: "auto",
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
-    <div className="flex flex-col fixed h-screen bg-[#76118f] py-2 px-4 text-xl w-[5%] items-center gap-20 z-50 ">
-      <NavLink to="/">
-        <img
-          className="s"
-          src="https://user-images.githubusercontent.com/100681165/238192169-5ed8ffdb-c8fb-4a69-8f91-d4d376271ae3.png"
-          alt="logo"
-        />
-      </NavLink>
+    <React.Fragment>
+      <div className="main-container">
+        <motion.div
+          animate={{
+            width: isOpen ? "240px" : "70px",
 
-      <div className="flex flex-col gap-6 pt-2">
-        <NavLink to="/">
-          <div className="group flex flex-col gap-3 justify-center items-center">
-            <FaHome className="text-[white]  hover:text-grape-100 group-hover:scale-125 duration-200" />
-            <h6 className="text-grape-100 text-xs opacity-0 group-hover:opacity-100 text-[white]">
-              Home
-            </h6>
+            transition: {
+              duration: 0.5,
+              type: "spring",
+              damping: 10,
+            },
+          }}
+          className={`sidebar `}
+        >
+          <div className="top_section">
+            <AnimatePresence>
+              {isOpen && (
+                <motion.h1
+                  variants={showAnimation}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  className="logo"
+                >
+                <img
+                className="s"
+                src="https://user-images.githubusercontent.com/100681165/238192169-5ed8ffdb-c8fb-4a69-8f91-d4d376271ae3.png"
+                alt="logo"
+                />
+                  <h1>WebResourcesHub</h1>
+                </motion.h1>
+              )}
+            </AnimatePresence>
+
+            <div className="bars">
+              <FaBars size={30} onClick={toggle} />
+            </div>
           </div>
-        </NavLink>
-        <NavLink to="/Videos">
-          <div className="group flex flex-col gap-3 justify-center items-center">
-            <BsFillCameraVideoFill className="text-[white] hover:text-grape-100 group-hover:scale-125 duration-200" />
-            <h6 className="text-grape-100 text-xs opacity-0 group-hover:opacity-100 text-[white]">
-              Videos
-            </h6>
-          </div>
-        </NavLink>
-        <NavLink to="/Websites">
-          <div className="group flex flex-col gap-3 justify-center items-center">
-            <CgWebsite className="text-[white] hover:text-grape-100 group-hover:scale-125 duration-200" />
-            <h6 className="text-grape-100 text-xs opacity-0 group-hover:opacity-100 text-[white]">
-              Websites
-            </h6>
-          </div>
-        </NavLink>
-        <NavLink to="/Challenges">
-          <div className="group flex flex-col gap-3 justify-center items-center">
-            <FaLaptopCode className="text-[white] hover:text-grape-100 group-hover:scale-125 duration-200" />
-            <h6 className="text-grape-100 text-xs opacity-0 group-hover:opacity-100 text-[white]">
-              Challenges
-            </h6>
-          </div>
-        </NavLink>
-        <NavLink to="/Tools">
-          <div className="group flex flex-col gap-3 justify-center items-center">
-            <BsTools className="text-[white] hover:text-grape-100 group-hover:scale-125 duration-200" />
-            <h6 className="text-grape-100 text-xs opacity-0 group-hover:opacity-100 text-[white]">
-              Tools
-            </h6>
-          </div>
-        </NavLink>
-        <NavLink to="/Books">
-          <div className="group flex flex-col gap-3 justify-center items-center">
-            <BsBookHalf className="text-[white] hover:text-grape-100 group-hover:scale-125 duration-200" />
-            <h6 className="text-grape-100 text-xs opacity-0 group-hover:opacity-100 text-[white]">
-              Books
-            </h6>
-          </div>
-        </NavLink>
+          
+          <section className="routes">
+            {routes.map((route, index) => {
+              if (route.subRoutes) {
+                return (
+                  <SidebarMenu
+                    setIsOpen={setIsOpen}
+                    route={route}
+                    showAnimation={showAnimation}
+                    isOpen={isOpen}
+                  />
+                );
+              }
+
+              return (
+                <NavLink
+                  to={route.path}
+                  key={index}
+                  className="link"
+                  activeClassName="active"
+                >
+                  <div id="ic" className="icon">{route.icon}</div>
+                  <AnimatePresence>
+                    {isOpen && (
+                      <motion.div
+                        variants={showAnimation}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="link_text"
+                      >
+                        {route.name}
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </NavLink>
+              );
+            })}
+          </section>
+        </motion.div>
+
+        <main>{children}</main>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
 export default Nav;
-

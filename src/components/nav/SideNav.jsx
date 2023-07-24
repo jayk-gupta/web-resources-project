9208f2cd69d01da72f0960afc045c31ec1d01ce4
Jay kumar gupta <jayesh.gupta3224@gmail.com>
2023-07-24 21:02:30 +0530

    home page modified

--- src/components/nav/SideNav.jsx
+++ src/components/nav/SideNav.jsx
@@ -6,12 +6,15 @@ function SideNav() {
 
   return (
     <div
-      className="flex flex-col fixed h-screen py-2 px-4 text-lg w-[15%]  gap-20 z-50  bg-gradient-to-b from-[#3f454a] to-[#1a1d20]
-     text-[#cacaca] overflow-scroll"
+      className=" flex flex-col fixed h-screen py-2 px-4 text-lg w-[15%]  gap-20 z-50  bg-gradient-to-b from-[#3f454a] to-[#1a1d20]
+     text-[#cacaca] "
+      style={{scrollbarColor: 'red'}}
     >
       <div className="search flex items-center gap-4 bg-[#2E2D2D] rounded-2xl px-2 py-[4px] mt-12">
         <AiOutlineSearch />
-        <p>Search...</p>
+        <input
+          style={{ background:'transparent',outline:'none'}}
+          placeholder="search..."></input>
       </div>
       {/* nav */}
       <div className="flex flex-col p-2  gap-6 ">

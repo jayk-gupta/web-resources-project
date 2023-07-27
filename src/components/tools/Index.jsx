26f7801db9aa9a4fb5202a124f02af03bf956d75
LostZoro476 <zoro80316@gmail.com>
2023-07-27 21:57:31 +0530

    Added Search Bar in the Tools Section

--- src/components/tools/Index.jsx
+++ src/components/tools/Index.jsx
@@ -9,6 +9,7 @@ import react from '../../database/tools/reactjs.json';
 const Index = () => {
   const [filter, setFilter] = useState('html');
   const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     if (filter === 'html') {
@@ -24,14 +25,43 @@ const Index = () => {
 
   const handleFilterChange = (target) => {
     setFilter(target);
+    setSearchTerm(''); 
   };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredData = data.filter((item) => {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   return (
     <div className='m-8 mt-32 lg:mt-8'>
+      <div className='mb-4 flex items-center'>
+        <input
+          type='text'
+          placeholder='Search tools...'
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className='border border-gray-400 px-4 py-2 rounded-lg w-80' 
+        />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='16'
+          height='16'
+          fill='currentColor'
+          viewBox='0 0 16 16'
+          className='h-5 w-5 ml-2 text-gray-600 cursor-pointer'
+          onClick={() => console.log('Search clicked')}
+        >
+          <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
+        </svg>
+      </div>
       <Filter onStateChange={handleFilterChange} />
       <div className='flex flex-wrap gap-5'>
-        {data.length > 0 ? (
-          data.map((res, i) => (
+        {filteredData.length > 0 ? (
+          filteredData.map((res, i) => (
             <Card
               key={res.title}
               title={res.title}

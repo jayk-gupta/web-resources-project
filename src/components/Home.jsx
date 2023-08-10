6c9d914671536b8b6e5d5167cc05d58a14285f10
avleen123 <mehak.bakshi201@gmail.com>
2023-08-10 22:29:21 +0530

    Type Writter Effect of Title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,54 @@
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [showText, setShowText] = useState(false);
+  const text = "Web Resources Hub";
+  const [currentText, setCurrentText] = useState("");
+  const fixedCharacter = "W";
+
+  const startTypewriter = () => {
+    let index = currentText.length;
+    const interval = setInterval(() => {
+      if (index < text.length) {
+        setCurrentText(text.substring(0, index + 1));
+        index++;
+      } else {
+        clearInterval(interval);
+        setTimeout(() => {
+          setCurrentText(fixedCharacter); // Set the fixed character
+          startTypewriter(); // Restart typewriter effect
+        }, 1000); // Pause before restarting
+      }
+    }, 350);
+  };
+
+  useEffect(() => {
+    setShowText(true);
+    setCurrentText(fixedCharacter); // Set the fixed character initially
+    startTypewriter();
+  }, []);
+
   return (
-    <div className="flex relative justify-start w-full h-screen mt-6 ">
+    <div className="flex relative justify-start w-full h-screen mt-6">
       <motion.div
-        className=" p-4 flex flex-col gap-8  ml-2 lg:ml-20 mt-20 lg:mt-8"
+        className="p-4 flex flex-col gap-8 ml-2 lg:ml-20 mt-20 lg:mt-8"
         initial={{ opacity: 0, translateX: -100 }}
         animate={{ opacity: 1, translateX: 0 }}
         transition={{ duration: 0.5 }}
       >
-        <h1 className="text-7xl text-[#444a4f] leading-[5rem] tracking-wider drop-shadow-xl ">
-          Web Resources Hub
+        <h1 className="text-7xl text-[#444a4f] leading-[5rem] tracking-wider drop-shadow-xl font-bold">
+          {currentText}
         </h1>
-        <h2 className="text-4xl text-[#3f454a] leading-[4rem] tracking-wider  drop-shadow-xl">
-          Get Empowered <span className=" text-[]"> with </span> <br />
+        <h2 className="text-4xl text-[#3f454a] leading-[4rem] tracking-wider drop-shadow-xl">
+          Get Empowered <span className="text-[]"> with </span> <br />
           Web Development
           <br />
-          <span className=" text-[]"> Resources </span>
+          <span className="text-[]"> Resources </span>
         </h2>
         <p className="text-lg w-4/5 text-[#858585]">
           Explore our vast collection of web development resources, designed to
-          give you the knowledge and tools you need to
-          succeed in the world of web development!
+          give you the knowledge and tools you need to succeed in the world of web development!
         </p>
       </motion.div>
     </div>
